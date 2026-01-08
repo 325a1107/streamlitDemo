2a8ac560962151a5e4ded5d676bbf0d94d7e945d
325a1107 <st325a1107y.gejo@urayama.ac.jp>
2026-01-08 09:44:53 +0900

    初回コミット

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import random
+
+#じゃんけんプログラム
+
+def jyanken (player_hand):
+    hands = ["グー","チョキ","パー"]
+    cpu_hand = random.choice(hands)
+    if player_hand == cpu_hand:
+        result = "あいこ！"
+    elif (player_hand == "グー" and cpu_hand == "チョキ") or \
+         (player_hand == "チョキ" and cpu_hand == "パー") or \
+         (player_hand == "パー" and cpu_hand == "グー"):
+        result = "勝利！"
+    else:
+        result = "負け！"
+    return cpu_hand, result
+
+
+#メインプログラム
+def main():
+    st.title("じゃんけんゲーム")
+    st.text("手を選んでください")
+    player_hand = st.selectbox("あなた",("グー","チョキ","パー"))
+    if st.button("じゃんけんポン！"):
+        cpu_hand, result = jyanken(player_hand)
+        st.write(f"CPU:{cpu_hand}")
+        st.write(f"結果:{result}")
+
+if __name__ == "__main__":
+    main()
